355eee6087687d15d5a105dec470e8eee2564dfd
SMohapatra3 <SMohapatra3@HIBACL25952.ad.harman.com>
2017-06-06 18:25:36 +0530

    addded selectAllPROPERTIES,selectChartPROPERTIES

--- wspUtill/src/main/java/com/unify_iri/wspUtill/config/WspAppCoreConfig.java
+++ wspUtill/src/main/java/com/unify_iri/wspUtill/config/WspAppCoreConfig.java
@@ -3,7 +3,6 @@ package com.unify_iri.wspUtill.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
